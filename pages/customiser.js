ac03e9fc09dc5ced58dc8979eef86b37decd610f
Ghiles Mekdam <ghilesmekdem@gmail.com>
2022-02-08 19:23:14 +0100

    started landing page

---
+++ pages/customiser.js
@@ -0,0 +1,60 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Canva from "../components/canvas";
+import Color from "../components/Color";
+import { useState } from "react";
+import { Provider } from "react-redux";
+import store from "../store/index";
+import Customizer from "../components/ControlPanel";
+import Header from "../components/header";
+import { useEffect } from "react";
+import Redirect from "../components/notLogged";
+import Link from "next/link";
+import { GiJetFighter } from "react-icons/gi";
+
+export default function Home() {
+  let [isLogged, setIsLogged] = useState(true);
+
+  useEffect(async () => {
+    let token = JSON.parse(localStorage.getItem("TOKEN"));
+    
+
+    fetch("http://localhost:840/getuser", {
+      method: "GET",
+      headers: {
+        Authtoken: token,
+      },
+    })
+      .then((response) => {
+        if (response.status == 200) {
+          setIsLogged(true);
+        } else if (response.status != 200) {
+          setIsLogged(false);
+        }
+      })
+      .then((data) => {
+        console.log(data)
+      });
+  }, []);
+
+  return (
+    <Provider store={store}>
+      {isLogged ? (
+        <div
+          className={styles.container}
+          style={{ padding: 0, display: "flex" }}
+        >
+          <Header />
+          <Canva />
+          <Customizer />
+          {/* <div style={{position:'absolute',height:"100vh", width:'50vw', left:0 , zIndex:'-1', backgroundImage:'url(background_4.jpg)'}}>
+
+          </div> */}
+        </div>
+      ) : (
+        <Redirect/>
+      )}
+    </Provider>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,89 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Canva from "../components/canvas";
-import Color from "../components/Color";
-import { useState } from "react";
-import { Provider } from "react-redux";
-import store from "../store/index";
-import Customizer from "../components/ControlPanel";
-import Header from "../components/header";
-import { useEffect } from "react";
-import Redirect from "../components/notLogged";
-import Link from "next/link";
-import { GiJetFighter } from "react-icons/gi";
+import { color, padding } from "@mui/system";
 
-export default function Home() {
-  let [isLogged, setIsLogged] = useState(true);
-
-  useEffect(async () => {
-    let token = JSON.parse(localStorage.getItem("TOKEN"));
-    
-
-    fetch("http://localhost:840/getuser", {
-      method: "GET",
-      headers: {
-        Authtoken: token,
-      },
-    })
-      .then((response) => {
-        if (response.status == 200) {
-          setIsLogged(true);
-        } else if (response.status != 200) {
-          setIsLogged(false);
-        }
-      })
-      .then((data) => {
-        console.log(data)
-      });
-  }, []);
-
+const index = () => {
   return (
-    <Provider store={store}>
-      {isLogged ? (
+    <div className="landing_main_container">
+      <div className="landing_header">
         <div
-          className={styles.container}
-          style={{ padding: 0, display: "flex" }}
+          style={{
+            width: "30%",
+            padding: " 1rem",
+            color: "white",
+          }}
         >
-          <Header />
-          <Canva />
-          <Customizer />
-          {/* <div style={{position:'absolute',height:"100vh", width:'50vw', left:0 , zIndex:'-1', backgroundImage:'url(background_4.jpg)'}}>
-
-          </div> */}
+          Brand Name
         </div>
-      ) : (
-        <Redirect/>
-      )}
-    </Provider>
+        <div
+          style={{
+            width: "40%",
+            padding: "1rem",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-end",
+          }}
+        >
+          <button
+            style={{
+              color: "white",
+              marginRight: "2rem",
+              width: "6rem",
+              height: "3rem",
+              border: "solid 1px #F6F7F9",
+              fontSize: "1rem",
+              borderRadius: "40px",
+            }}
+          >
+            {" "}
+            Sign Up
+          </button>
+          <button
+            style={{
+              color: "white",
+              marginRight: "2rem",
+              width: "6rem",
+              height: "3rem",
+              fontSize: "1rem",
+              backgroundColor: "#24292E",
+              borderRadius: "0.375rem",
+            }}
+          >
+            {" "}
+            Login{" "}
+          </button>
+        </div>
+      </div>
+
+      <div
+        className="landing_title_box"
+      >
+        <p>Your Style,</p>
+        <p>Your Creation,</p>
+        <div style={{
+            marginLeft:'2rem',
+            display:'flex'
+        }}>
+          <p style={{
+              fontFamily:'Raleway',
+              fontSize:'4rem',
+              fontWeight:'800'
+          }}>Simply, </p>
+          <p style={{
+              fontFamily:'Raleway',
+              fontSize:'4rem',
+              fontWeight:'800',
+              marginLeft:'1rem',
+              color:"#00897B"
+          }}>YOU</p>
+        </div>
+      </div>
+      <div className="landing_text_box">
+          <p>We provide the best quality shirts and hoodies, and let the styling to you !</p><br/>
+          <p>Using our online customisation tool, you can choose the color and prints on your clothing and we take care of the rest !</p><br/>
+          <p>We only use bio-degradable and eco-friendly materials, you even have a 2 weeks warranty and exchange is completely free of charge !</p><br/>
+          <p>You can start creating by clicking on the button below to create an account, jump in !</p><br/>
+      </div>
+    </div>
   );
-}
+};
+
+export default index;
